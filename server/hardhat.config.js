cdf5f83c6fa2865e3dac165d3f0c2e0d64ed106a
Elleuch kais <cherif.hela20@gmail.com>
2023-04-10 16:16:06 +0200

    Add packages for ci/cd

--- server/hardhat.config.js
+++ server/hardhat.config.js
@@ -18,7 +18,7 @@ module.exports = {
         },
     },
     solidity: {
-        version: "0.8.19",
+        version: "0.8.15",
         settings: {
             optimizer: {
                 enabled: true,
