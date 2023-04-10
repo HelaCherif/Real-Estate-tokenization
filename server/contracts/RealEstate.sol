8d94e78398f5beed5e2055c9229ba068e5fec94f
Elleuch kais <cherif.hela20@gmail.com>
2023-04-10 11:04:09 +0200

    Add CI/CD pipeline and some fixes

--- server/contracts/RealEstate.sol
+++ server/contracts/RealEstate.sol
@@ -1,167 +1,273 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/Base64.sol";
-import "hardhat/console.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import '../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
-
+/**
+ * @title RealEstate
+ * @notice Real Estate Properties Tokenization contract
+ * @dev RealEstate contract is an implementation of ERC721 openzeppelin contract to mint and transfer NFTs
+ * @author Hela CHERIF
+ */
 contract RealEstate is Ownable, ERC721 {
     using Counters for Counters.Counter;
-
     Counters.Counter private _tokenIds;
     Counters.Counter private _propertyId;
 
+    /// @dev Structure of Property
     struct Property {
         uint id;
-        uint256 tokenNumber;
-        uint256 postalCode;
-        uint256 annualYield;
-        uint256 price;
-        bool isPublished;
         string name;
+        string description;
+        bool isPublished;
+        PropertyAddress propertyAddress;
+        FinancialInfos financialInfos;
+    }
+
+    /// @dev Structure of PropertyAddress
+    struct PropertyAddress {
+        uint256 postalCode;
         string addr;
         string city;
         string complementAddress;
-        string description;
     }
 
+    /// @dev Structure of FinancialInfos
+    struct FinancialInfos {
+        uint256 tokensNumber;
+        uint256 annualYield;
+        uint256 propertyPrice;
+        uint256 tokenPrice;
+    }
+
+    /// @dev Structure of Token
     struct Token {
-        uint tokenId;
-        address addr;
-        string name;
+        uint id;
         uint propertyId;
+        address ownerAddress;
+        string name;
         string url;
     }
 
-    constructor() ERC721("RealEstate", "RealEstate") payable {}
-
-
+    mapping(address => Token[]) public clientsTokens;
     Property[] public allProperties;
     Token[] public allTokens;
-    mapping(address => Token[]) public clients;
-    uint256 public constant MAX_MINT_PER_TX = 10;
-    //owner of NFT Token and Marketplace are same.
-    address nftMarketAddress; //address of the NFT marketplace
 
 
-    event PropertyAdded(uint id, string name, uint price, string addr, uint tokenNumber);
-    event PropertyUpdated(uint id, string name, uint price, string addr, uint tokenNumber);
-    event PropertyDeleted(uint id);
-    event PropertyPublished(uint id);
-    event MintToken(address indexed _to, uint256 quantity,uint tokenId);
-    event Mint(address indexed _to, uint256 quantity);
+    // ::::::::::::::::::::::::::::: CONSTRUCTOR :::::::::::::::::::::::::: //
 
-    function _baseURI() internal view virtual override returns (string memory) {
-        return "https://ipfs.io/ipfs/QmSrSwboxekwhUfK5nKcbzK6xuTmNxhsiz643pmjqJfqPt/";
+    constructor() ERC721("RealEstate", "RealEstate") payable {}
+
+    // ::::::::::::::::::::::::::::::: GETTERS ::::::::::::::::::::::::::::: //
+
+    /**
+     * @dev Get property
+     * @param _id of the property
+     * @return property
+     */
+    function getProperty(uint _id) private view returns (Property memory) {
+        return allProperties[_id];
     }
 
-    function baseURI() public view returns (string memory) {
-        return _baseURI();
+    /**
+     * @dev Get Token
+     * @param _id of the token
+     * @return token
+     */
+    function getToken(uint _id) private view returns (Token memory) {
+        return allTokens[_id];
     }
 
-    fallback() external payable {
-        // custom function code
-    }
+    // ::::::::::::::::::::::::::::::: EVENTS :::::::::::::::::::::::::::::: //
 
-    receive() external payable {
-        // custom function code
-    }
+    /**
+     * @dev Emit an event when the property is added
+     * @param _id of the property
+     * @return Event PropertyAdded
+     */
+    event PropertyAdded(uint _id);
 
+    /**
+     * @dev Emit an event when the property is updated
+     * @param _id of the property
+     * @return Event PropertyUpdated
+     */
+    event PropertyUpdated(uint _id);
+
+    /**
+     * @dev Emit an event when the property is deleted
+     * @param _id of the property
+     * @return Event PropertyDeleted
+     */
+    event PropertyDeleted(uint _id);
+
+    /**
+     * @dev Emit an event when the property is published (ready to be minted)
+     * @param _id of the property
+     * @return Event PropertyPublished
+     */
+    event PropertyPublished(uint _id);
+
+    /**
+     * @dev Emit an event when the Token NFT is minted
+     * @param _to of address of the owner
+     * @param _tokenId of the Token
+     * @return Event MintToken
+     */
+    event MintToken(address indexed _to, uint _tokenId);
+
+    /**
+     * @dev Emit an event when the Property is fully NFT minted
+     * @param _to of address of the owner
+     * @param _tokens list of tokens minted
+     * @return Event MintProperty
+     */
+    event MintProperty(address indexed _to, Token[] _tokens);
+
+    /**
+     * @dev Emit an event when the NFT token is transferred
+     * @param _to of address of the owner
+     * @param _tokenId transferred
+     * @return Event TransferToken
+     */
+    event TransferToken(address indexed _to, uint _tokenId);
+
+    /**
+    * @dev Emit an event when a new client is added
+     * @param _addr transferred
+     * @return Event ClientAdded
+     */
+    event ClientAddedAndApproved(address _addr);
+
+    // :::::::::::::::::::::::::::: FUNCTIONS :::::::::::::::::::::::: //
+
+
+    /**
+     * @dev Add a new property only by the owner , the id is generated by the contract
+     * @param _property of proposal
+     */
     function addProperty(Property memory _property) external onlyOwner {
-        require(keccak256(abi.encode(_property.name)) != keccak256(abi.encode("")), 'The name of the property is mandatory');
+        require(keccak256(abi.encode(_property.name)) != keccak256(abi.encode("")),
+            'The name of the property is mandatory');
+        require(_property.financialInfos.propertyPrice > 0,
+            'The price of the property should be superior to 0 ');
+        require(_property.financialInfos.tokensNumber > 0,
+            'The token number of the property should be superior to 0 ');
         _property.id = _propertyId.current();
         allProperties.push(_property);
         _propertyId.increment();
-        emit PropertyAdded(_property.id, _property.name, _property.price, _property.addr, _property.tokenNumber);
+        emit PropertyAdded(_property.id);
     }
 
+    /**
+     * @dev Update an existing property only by the owner
+     * @param _property of proposal
+     */
+    function updateProperty(Property memory _property) external onlyOwner {
+        require(_property.id < allProperties.length, 'The _property does not exist');
+        allProperties[_property.id] = _property;
+        emit PropertyUpdated(_property.id);
+    }
+
+    /**
+     * @dev Delete an existing property only by the owner
+     * @param _id of the property
+     */
     function deleteProperty(uint256 _id) external onlyOwner {
         delete allProperties[_id];
         emit PropertyDeleted(_id);
     }
 
-    function updateProperty(Property memory _property) external onlyOwner {
-        allProperties[_property.id] = _property;
-        emit PropertyUpdated(_property.id, _property.name, _property.price, _property.addr, _property.tokenNumber);
-    }
-
+    /**
+     * @dev Publish an existing property only by the owner
+     * @param _id of the property
+     */
     function publishProperty(uint256 _id) external onlyOwner {
         allProperties[_id].isPublished = true;
         emit PropertyPublished(_id);
     }
 
-    function mintNFTProperty(uint256 _propertyId) payable external onlyOwner {
-        require(allProperties[_propertyId].isPublished, "The property is not published yet");
-        Property memory property = allProperties[_propertyId];
-        for (uint i = 0; i < property.tokenNumber; i++) {
-            _safeMint(msg.sender, _tokenIds.current());
-            //setTokenMetadata(_tokenIds.current(), "NFT" , baseURI());
-            allTokens.push(Token(_tokenIds.current(), msg.sender, "NFT",_propertyId, baseURI()));
-            emit MintToken(msg.sender, property.tokenNumber,_tokenIds.current());
-            _tokenIds.increment();
-            setApprovalForAll(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, true);
-        }
+    // :::::::::::::::::::::::::: FUNCTIONS ERC721 :::::::::::::::::::::::: //
 
-        emit Mint(msg.sender, property.tokenNumber);
+    /**
+     * @dev Mint all the tokens of an existing property only by the owner
+     * @param _id : id of the property to be minted
+     */
+    function mintProperty(uint256 _id) payable external onlyOwner {
+        Property memory property = getProperty(_id);
+        require(property.isPublished, "The property is not published yet");
+        // Mint all the tokens of the property
+        for (uint i = 0; i < property.financialInfos.tokensNumber; i++) {
+            _safeMint(msg.sender, _tokenIds.current());
+            Token memory token;
+            token.id = _tokenIds.current();
+            token.ownerAddress = msg.sender;
+            token.name = " Real Estate NFT";
+            token.propertyId = _id;
+            token.url = baseURI();
+            allTokens.push(token);
+            emit MintToken(msg.sender, _tokenIds.current());
+            _tokenIds.increment();
+        }
+        emit MintProperty(msg.sender, allTokens);
     }
 
-    // Transfers ERC721 token from the caller's address to another address
+    /**
+     * @dev Add a new client and approved it by the owner
+     * @param _addr of the client
+     */
+    function approveClient(address _addr) internal onlyOwner {
+        setApprovalForAll(_addr, true);
+        emit ClientAddedAndApproved(_addr);
+    }
+
+    /**
+     * @dev Transfers ERC721 token from owner to client
+     * @param _to address of the client
+     * @param _tokenId of the token
+     */
+    //
     function transferTo(address _to, uint256 _tokenId) public payable {
-        Token memory token = allTokens[_tokenId];
-        Property memory property = allProperties[token.propertyId];
-        require(msg.value >= property.price , "Insufficient Funds");
+        Token memory token = getToken(_tokenId);
+        Property memory property = getProperty(token.propertyId);
+        require(msg.value >= property.financialInfos.tokenPrice, "Insufficient Funds");
+        // First transaction for this client so we should approved him
+        if (clientsTokens[_to].length == 0) {
+            approveClient(_to);
+        }
+        clientsTokens[_to].push(token);
         approve(_to, _tokenId);
         safeTransferFrom(msg.sender, _to, _tokenId, "0x");
-        allTokens[_tokenId].addr=_to;
-        token.addr=_to;
-        clients[_to].push(token);
+        //Change the owner of the token
+        allTokens[_tokenId].ownerAddress = _to;
+        token.ownerAddress = _to;
+        clientsTokens[_to].push(token);
+        emit TransferToken(_to, _tokenId);
     }
 
-    function setTokenMetadata(
-        uint256 tokenId,
-        string memory name,
-        string memory url
-    ) internal {
-        allTokens[tokenId] = Token(tokenId,msg.sender,name,1, url);
+    // :::::::::::::::::::::::::::::::::::: OTHER FUNCTIONS  ::::::::::::::::::::::::::::::::::::: //
+
+    fallback() external payable {
     }
 
-    function tokenURI(uint256 tokenId)
-    public
-    view
-    override(ERC721)
-    returns (string memory)
-    {
-        string memory json = Base64.encode(
-            bytes(
-                string(
-                    abi.encodePacked(
-                        '{"name": "',
-                        allTokens[tokenId].name,
-                        '",',
-                        '"image_data": "',
-                        allTokens[tokenId].url,
-                        '",',
-                        '"attributes": [{"trait_type": "Speed", "value": ',
-                        "5",
-                        "},",
-                        '{"trait_type": "Attack", "value": ',
-                        "2",
-                        "},",
-                        '{"trait_type": "Defence", "value": ',
-                        "3",
-                        "},",
-                        '{"trait_type": "Material", "value": "',
-                        "4",
-                        '"}',
-                        "]}"
-                    )
-                )
-            )
-        );
-        return string(abi.encodePacked("data:application/json;base64,", json));
+    receive() external payable {
+    }
+    /**
+    * @dev Get IPFS Address
+     * @return url
+     */
+    function _baseURI() internal view virtual override returns (string memory) {
+        return "https://ipfs.io/ipfs/QmSrSwboxekwhUfK5nKcbzK6xuTmNxhsiz643pmjqJfqPt/";
     }
 
+    /**
+     * @dev Get Base URL
+     * @return url
+     */
+    function baseURI() public view returns (string memory) {
+        return _baseURI();
+    }
 
 }
--- server/hardhat.config.js
+++ server/hardhat.config.js
@@ -1,12 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv').config();
+require("hardhat-gas-reporter");
+const {INFURA_ID} = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     networks: {
         localhost: {
             url: "http://127.0.0.1:8545",
             chainId: 31337,
-        }
+        },
+        sepolia: {
+            url: `https://sepolia.infura.io/v3/${INFURA_ID}`,
+            chainId: 11155111,
+        },
+        goerli: {
+            url: `https://goerli.infura.io/v3/${INFURA_ID}`,
+            chainId: 5,
+        },
     },
     solidity: {
         version: "0.8.19",
@@ -17,4 +27,8 @@ module.exports = {
             }
         }
     },
+    gasReporter: {
+        currency: 'CHF',
+        gasPrice: 21
+    }
 };
